858d86e988a50a14d84da4fe8edebde57771742e
Rastko <rastko_jovic@outlook.com>
2024-07-18 13:15:29 +0200

    Update carousel-main.js

--- scripts/carousel-main.js
+++ scripts/carousel-main.js
@@ -1,42 +1,39 @@
 let slideNumber = 0;
+let slides = document.getElementsByClassName("slide");
+let dotNavigationMenu = document.querySelector(".dot-navigation-menu");
 showSlides(slideNumber);
 
-function plusSlides(n) {
-  slideNumber += n;
-  showSlides(slideNumber);
+function displayCurrentSlideDot(dotIndex) {
+  for (let i = 0; i < dotNavigationMenu.children.length; i++) {
+    dotNavigationMenu.children[i].classList.remove("color-transition");
+    void dotNavigationMenu.children[i].offsetWidth;
+  }
+
+  console.log(`Adding class to: ${dotNavigationMenu.children[dotIndex]}!`);
+  dotNavigationMenu.children[dotIndex].classList.add("color-transition");
 }
 
-function showSlides(n) {
-  let slides = document.getElementsByClassName("slide");
-
-  if (n > slides.length - 1) {
+function plusSlides(increment) {
+  slideNumber += increment;
+  if (slideNumber > slides.length - 1) {
     slideNumber = 0;
   }
 
-  if (n < 0) {
+  if (slideNumber < 0) {
     slideNumber = slides.length - 1;
   }
+  showSlides(slideNumber);
+}
+
+function showSlides(slideIndex) {
+  slideNumber = slideIndex;
 
   for (let i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
 
-  slides[slideNumber].style.display = "flex";
+  slides[slideIndex].style.display = "flex";
+  displayCurrentSlideDot(slideIndex);
 }
 
-function autoCarousel() {
-  let slides = document.getElementsByClassName("slide");
-
-  for (let i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-
-  slideNumber++;
-  if (slideNumber > slides.length) {
-    slideNumber = 1;
-  }
-
-  slides[slideNumber - 1].style.display = "flex";
-
-  setTimeout(autoCarousel, 3000);
-}
+export { plusSlides, slideNumber };
